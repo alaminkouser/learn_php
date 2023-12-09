5c933a3e4cf12426291ad3af3e01ce9a033c6953
AL AMIN KOUSER <alaminkouser@icloud.com>
2023-12-09 20:41:07 +0600

    writing html

--- site.php
+++ site.php
@@ -3,13 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo "Title" ?></title>
 </head>
 <body>
     <?php 
-    
-    echo("Hello World!");
-    
+    echo("<h1>Header 1</h1>");
     ?>
 </body>
 </html>
